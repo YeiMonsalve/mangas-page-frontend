1995fe721f4c95699482ddf8cdafbe9e3a46a650
Yeison Monsalve <168552590+YeiMonsalve@users.noreply.github.com>
2024-08-27 19:23:13 -0500

    correción y ajustes de algunos errores

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,38 +1,32 @@
+// Poner el volumend el manga como principal
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Seleccionar todos los elementos del manga
   const mangaItems = document.querySelectorAll('.manga-item');
 
-  // Recorrer cada elemento del manga y agregar un event listener a los volúmenes dentro de cada manga
   mangaItems.forEach(manga => {
-    // Seleccionar los elementos de volumen dentro del manga actual
     const volumeItems = manga.querySelectorAll('.volume-item');
     const mangaImg = manga.querySelector('img');
     const mangaTitle = manga.querySelector('.tittle-span');
     const mangaDescription = manga.querySelector('.title-descrition');
     const mangaPrice = manga.querySelector('.main-Price')
 
-    // Recorrer cada elemento del volumen dentro del manga actual y agregar un event listener
     volumeItems.forEach(item => {
       item.addEventListener('click', function () {
-        // Obtener la imagen, texto, descripción y precio del volumen seleccionado
         const imgSrc = item.querySelector('img').src;
         const volText = item.querySelector('span').textContent;
         const volDescription = item.querySelector('p.description').textContent;
         const volPrice = item.querySelector('span.price').textContent;
 
-        // Guardar la imagen, texto y descripción original del manga actual
         const originalImgSrc = mangaImg.src;
         const originalVolText = mangaTitle.textContent;
         const originalDescription = mangaDescription.textContent;
         const originalPrice = mangaPrice.textContent;
 
-        // Actualizar la imagen, texto y descripción del manga actual con el volumen seleccionado
         mangaImg.src = imgSrc;
         mangaTitle.textContent = volText;
         mangaDescription.textContent = volDescription;
         mangaPrice.textContent = volPrice;
 
-        // Restaurar la imagen, texto y descripción del volumen seleccionado con el manga principal original
         item.querySelector('img').src = originalImgSrc;
         item.querySelector('span').textContent = originalVolText;
         item.querySelector('p.description').textContent = originalDescription;
@@ -41,3 +35,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// buscador del catalogo
+
+function searchManga() {
+
+  const searchTerm = document.querySelector('#search-input').value.trim().toLowerCase();
+  const mangaItems = document.querySelectorAll('.manga-item');
+  const searchInput = document.querySelector('#search-input');
+
+  searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      searchManga();
+    }
+  });
+
+  mangaItems.forEach(item => {
+    const mangaTitle = item.querySelector('h2').innerText.trim().toLowerCase();
+
+    if (mangaTitle.includes(searchTerm)) {
+      item.classList.remove('hidden');
+    } else {
+      item.classList.add('hidden');
+    }
+  });
+}
+
+//filtrar por mangas
+
+function filtrarMangas() {
+  var categoriaSeleccionada = document.getElementById("category-select").value;
+  var mangas = document.querySelectorAll(".manga-item");
+
+  mangas.forEach(function (manga) {
+      var categoriaManga = manga.getAttribute("data-categoria");
+      if (categoriaSeleccionada === "todos" || categoriaManga === categoriaSeleccionada) {
+          manga.classList.remove("hidden");
+      } else {
+          manga.classList.add("hidden");
+      }
+  });
+}
--- src/scripts/header_Menu_Submenu.js
+++ src/scripts/header_Menu_Submenu.js
@@ -1,3 +1,16 @@
+// menu
+function adjustContainerPosition() {
+    const header = document.querySelector('header');
+    const container = document.getElementById('menuIconDesp');
+    container.style.top = header.offsetHeight + 'px';
+}
+
+window.addEventListener('load', adjustContainerPosition);
+window.addEventListener('resize', adjustContainerPosition);
+
+document.querySelector('header').addEventListener('transitionend', adjustContainerPosition);
+
+// submenu
 document.addEventListener('DOMContentLoaded', () => {
     const profileHeaderLink = document.getElementById('profile-header-link');
     const submenu = document.getElementById('submenu');
